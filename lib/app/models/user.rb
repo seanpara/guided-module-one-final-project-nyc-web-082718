e6e8d967fea5e02477b71ef7f487ed5cf1f2f32e
seanpara <spara77@gmail.com>
2018-09-12 17:58:55 -0400

    finished add to favorites, started delete from favorites

--- lib/app/models/user.rb
+++ lib/app/models/user.rb
@@ -29,7 +29,13 @@ class User < ActiveRecord::Base
     def add_meal_to_favorites(meal)
       #I want to be able to add a meal i like to my favorites list as well as remove them
 
-      Favorite.new(self, meal)
+      Favorite.create({user_id: self.id, meal_id: meal.id})
+    end
+
+    def delete_meal_from_favorites(meal)
+
+      meal_to_delete = Favorite.find_by(meal_id: meal.id)
+      meal_to_delete.destroy
     end
 
     def prepare_meal(meal)
@@ -44,12 +50,12 @@ class User < ActiveRecord::Base
 
     def available_meals
       # medium : I want to be able enter my name and check which types of meals are vailable based on my inventory
-
+      #cross reference all meals against my kitchen
     end
 
-    def neccessary_ingredients
+    def neccessary_ingredients(meal)
       # medium: I want to be able to know which ingredients I'm missing in ordered to make a specified meal
-
+      self.kitchen
     end
 
     # hard: I want to be able to find all the recipes I am only missing one ingredient for
