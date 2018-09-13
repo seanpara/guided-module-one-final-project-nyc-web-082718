73682d9f05faba85f685bd286fb817572d8c981c
Chris Stull <chris.stull@gmail.com>
2018-09-13 15:32:15 -0400

    shellshock

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -16,11 +16,7 @@ def identify
   until @@new_or_not == "new user" || @@new_or_not == "returning user"
     puts "That is not a valid response. Please type 'new_user' or 'returning_user' exactly as shown."
     @@new_or_not = gets.chomp
-<<<<<<< HEAD
   new_or_not
-=======
-  end
->>>>>>> 4a78504dfdaeed1e9de16e82f9ea28b77ba52e22
 end
 
 def id_valid?(id)
@@ -149,7 +145,6 @@ def run_command_line_interface
   identify
   set_up_user
   topic?
-<<<<<<< HEAD
 
 
 def meal_recommendation(user)
@@ -198,6 +193,7 @@ def meal_recommendation(user)
         puts "="*25
       end
     end
+    #meal rec based on meal time
   elsif selection == 'meal time'
     puts "Which meal time do you want a suggestion for? Choose breakfast, lunch, or dinner"
     meal_time = gets.chomp
@@ -211,6 +207,7 @@ def meal_recommendation(user)
       "#{option.name}"
     end
   end
+end
 
   def continue_or_quit?
     puts "Do you want to continue or quit?"
@@ -222,67 +219,3 @@ def meal_recommendation(user)
     end
   end
 end
-=======
-end
-
-
-# def meal_recommendation(user)
-#   puts "How do you want to pick a meal?"
-#   puts "Please select 'calories','macros', or 'meal time'"
-#   selection = gets.chomp
-#
-#   until selection in ['calories','macros','meal time']
-#     puts "I can't recommend a meal based on that. Please select from 'calories', 'macros', or 'meal time'"
-#     selection = gets.chomp
-#   end
-#
-#   #meal rec based on calories
-#   if selection == 'calories'
-#     puts "What is the calorie limit for this meal?"
-#     limit = gets.chomp.to_i
-#     until limit.is_a? Integer
-#       puts "I need a number to recommend based on calories. Please enter the calorie limit for this meal."
-#       limit = gets.chomp
-#     end
-#     meal_options = user.meal_request_based_on_calories(limit)
-#     puts "I can recommend the following meals to you:"
-#     meal_options.map do |option|
-#       puts "#{option.name}, a #{option.what_category?} meal with #{option.calories} calories"
-#     end
-#     #meal rec based on macros
-#   elsif selection == 'macros'
-#     puts "Which macro would you like to specify: protein, fats, or carbohydrates?"
-#     macro = gets.chomp
-#     until macro in ['protein','fats','carbohydrates']
-#       puts "That's not a valid macro. Please pick from protein, fats, or carbohydrates."
-#       macro = gets.chomp
-#     end
-#     puts "How many grams of #{macro} are you looking for?"
-#     amount = gets.chomp.to_i
-#     until amount.is_a? Integer
-#       puts "I need a number amount of #{macro} to make a recommendation. How many grams would you like?"
-#       amount = gets.chomp.to_i
-#     end
-#     meal_options = user.suggest_meal_by_macros(macro,amount)
-#     puts "I can recommend the following meals to you:"
-#     meal_options.map do |option|
-#       puts "#{option.name}, a #{option.calories}-calorie #{option.what_category?} meal with:"
-#       option.nutrients.each do |nutrient,amount|
-#         puts "#{amount} grams of #{nutrient}"
-#         puts "="*25
-#       end
-#     end
-#   elsif selection == 'meal time'
-#     puts "Which meal time do you want a suggestion for? Choose breakfast, lunch, or dinner"
-#     meal_time = gets.chomp
-#     until meal_time in ["breakfast","lunch","dinner"]
-#       puts "That wasn't a valid selection. Choose breakfast, lunch, or dinner"
-#       meal_time = gets.chomp
-#     end
-#     meal_options = user.suggest_meal_by_time
-#     puts "I can recommend the following meals to you:"
-#     meal_options.map do |option|
-#       "#{option.name}"
-#     end
-#   end
->>>>>>> 4a78504dfdaeed1e9de16e82f9ea28b77ba52e22
